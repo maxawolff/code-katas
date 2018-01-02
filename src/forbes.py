117a4d7a299a99809e0da9d85c98ffae0ab44d5c
maxawolff <maxawolff@hotmail.com>
2018-01-02 14:07:27 -0800

    return info on billionaires correctly

--- src/forbes.py
+++ src/forbes.py
@@ -16,15 +16,21 @@ def billionaires():
     bdata = json_to_dict('billionaires.json')
     youngest_valid_age = 100
     oldest_age_under_80 = 0
-    youngest_billionare = 0
-    oldest_billionare = 0
+    youngest = 0
+    oldest = 0
     for person in bdata:
         if person['age'] > oldest_age_under_80 and person['age'] < 80:
             oldest_age_under_80 = person['age']
-            oldest_billionare = person['name']
+            oldest = person
         if person['age'] < youngest_valid_age and person['age'] > 0:
             youngest_valid_age = person['age']
-            youngest_billionare = person['name']
-    pdb.set_trace()
-    # youngest_info = [youngest_billionare]
-    # oldest_info = []
+            youngest = person
+    youngest_info = [youngest['name'],
+                     youngest['net_worth (USD)'],
+                     youngest['source']]
+    oldest_info = [oldest['name'],
+                   oldest['net_worth (USD)'],
+                   oldest['source']]
+    bill_info = {'Youngest Billionaire': youngest_info,
+                 'Oldest Billionaire under 80': oldest_info}
+    return bill_info
---
+++ src/test_forbes.py
@@ -0,0 +1,7 @@
+"""Test file for forbes.py."""
+from forbes import billionaires
+
+
+def test_forbes_returns_correct_names():
+    """Test that the names of the billionares are correct."""
+    billionaires()
