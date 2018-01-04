015d8e71b33899ed5556dda9d9c77e2bfee50762
maxawolff <maxawolff@hotmail.com>
2018-01-04 11:05:13 -0800

    finished testing for all functions, cleaned up doc strings

--- src/test_string_pyramid.py
+++ src/test_string_pyramid.py
@@ -10,23 +10,38 @@ above = [(None, None), ('', ''), ('*#', '***\n*#*\n***'),
          ('54321', '555555555\n544444445\n543333345\n543222345\n543212345\n543222345\n543333345\n544444445\n555555555')]
 
 
+side = [(None, None), ('', ''), ('*#', ' # \n***'),
+        ('abc', '  c  \n bbb \naaaaa'), ('aaa', '  a  \n aaa \naaaaa'),
+        ('54321', '    1    \n   222   \n  33333  \n 4444444 \n555555555')]
+
+
+visible = [(None, -1), ('', -1), ('*#', 9),
+           ('abc', 25), ('aaa', 25), ('54321', 81)]
+
+
+total = [(None, -1), ('', -1), ('*#', 10),
+         ('abc', 35), ('aaa', 35), ('54321', 165)]
+
+
 @pytest.mark.parametrize('chars, res', above)
 def test_from_above(chars, res):
-    """Test dat function."""
+    """Test view pyramid from above."""
     assert watch_pyramid_from_above(chars) == res
 
 
-def test_from_side():
-    """."""
-    res = watch_pyramid_from_the_side('aaa')
-    pass
+@pytest.mark.parametrize('chars, res', side)
+def test_from_side(chars, res):
+    """Test view pyramid from the side."""
+    assert watch_pyramid_from_the_side(chars) == res
 
 
-def test_num_characters_visible():
-    """."""
-    pass
+@pytest.mark.parametrize('chars, res', visible)
+def test_visible(chars, res):
+    """Test count visible characters."""
+    assert count_visible_characters_of_the_pyramid(chars) == res
 
 
-def test_num_characters_total():
-    """."""
-    pass
+@pytest.mark.parametrize('chars, res', total)
+def test_total(chars, res):
+    """Test count all characters."""
+    assert count_all_characters_of_the_pyramid(chars) == res
